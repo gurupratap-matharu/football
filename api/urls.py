c7a3f9d66429b7e7f494888b68b1329bfd00c7aa
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-19 16:05:30 -0300

    Hook up import league view and endpoint 🪝

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CompetitionAPIView
+from .views import CompetitionAPIView, CompetitionDetailView, import_league
 
 urlpatterns = [
     path("", CompetitionAPIView.as_view(), name="competition_list"),
+    path("<int:pk>/", CompetitionDetailView.as_view(), name="competition_detail"),
+    path("import-league/<str:code>/", import_league, name="import_league"),
 ]
