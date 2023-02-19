1072b508e8600865c53cbf253a5194db878fcaa6
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-19 16:36:34 -0300

    Introduce data modelling for Team Player and League

--- api/models.py
+++ api/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Area(models.Model):
+    """
+    Represents a particular geographical area or location where league matches are
+    being played.
+    """
+
     name = models.CharField(max_length=250)
 
     def __str__(self):
@@ -9,30 +14,48 @@ class Area(models.Model):
 
 
 class Competition(models.Model):
+    """
+    Also known as a league is an event taking place at an area where many teams participate
+    to play matches against each other.
+    """
+
     name = models.CharField(max_length=250)
     code = models.CharField(max_length=10)
-    area = models.ForeignKey("Area", on_delete=models.CASCADE)
+    area = models.ForeignKey(
+        "Area", on_delete=models.CASCADE, related_name="competitions"
+    )
+    teams = models.ManyToManyField(to="Team", related_name="running_competitions")
 
     def __str__(self):
         return f"{self.name}"
 
 
-# class Team(models.Model):
-#     name = models.CharField(max_length=250)
-#     tla = models.CharField(max_length=250)
-#     short_name = models.CharField(max_length=100)
-#     address = models.CharField(max_length=250)
-#     area = models.ForeignKey("Area", on_delete=models.CASCADE)
+class Team(models.Model):
+    """
+    An ensemble of players also known as squad, a coach which participate in a
+    competition or a league.
+    """
 
-#     def __str__(self):
-#         return f"{self.name}"
+    name = models.CharField(max_length=250)
+    tla = models.CharField(max_length=20)
+    short_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    area = models.ForeignKey("Area", on_delete=models.CASCADE, related_name="teams")
+    squad = models.ManyToManyField(to="Player", related_name="teams")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
-# class Player(models.Model):
-#     name = models.CharField(max_length=250)
-#     position
-#     date_of_birth
-#     nationality
+class Player(models.Model):
+    """
+    A physical sports person who plays the match.
+    """
 
-#     def __str__(self):
-#         return f"{self.name}"
+    name = models.CharField(max_length=250)
+    position = models.CharField(max_length=200)
+    date_of_birth = models.CharField(max_length=20)
+    nationality = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
