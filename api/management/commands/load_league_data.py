4109659400ee81aa917017772b41fa50f25ed910
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-19 16:04:29 -0300

    Implement management command to fetch data from API

---
+++ api/management/commands/load_league_data.py
@@ -0,0 +1,51 @@
+"""A utility script to load league data into the db for the Football api.
+
+Running this script would populate the following db models
+    - Competitions (Leagues)
+    - Teams
+    - Players
+    - Coaches
+
+with fresh data fetched from the API
+"""
+
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Management command which populates database with Football API data
+    """
+
+    help = "Fetch league data from the API and populate the database"
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument(
+            "-l",
+            "--league",
+            type=str,
+            help="The league for which the data is to be fetched.",
+        )
+
+    def success(self, msg):
+        self.stdout.write(self.style.SUCCESS(msg))
+
+    def danger(self, msg):
+        self.stdout.write(self.style.HTTP_BAD_REQUEST(msg))
+
+    def handle(self, *args, **kwargs):
+        league = kwargs.get("league")
+
+        self.success("League: %s" % league)
+        self.success("Fetching data from api 📡...")
+
+        self.stdout.write(
+            f"""
+        Competitions : xxx
+        Teams     : xxx
+        Players     : xxx
+        """
+        )
+
+        self.success("All done! 💖💅🏻💫")
--- api/views.py
+++ api/views.py
@@ -1,9 +1,29 @@
+import logging
+
+from django.core.management import call_command
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 
 from .models import Competition
 from .serializers import CompetitionSerializer
 
+logger = logging.getLogger(__name__)
+
+
+@csrf_exempt
+def import_league(request, code=None):
+    """Loads league data into the database"""
+
+    call_command("load_league_data", league=code)
+    return HttpResponse("All ok")
+
 
 class CompetitionAPIView(generics.ListAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+
+class CompetitionDetailView(generics.RetrieveAPIView):
     queryset = Competition.objects.all()
     serializer_class = CompetitionSerializer
