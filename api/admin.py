5529f66ee2f336ea55ed013ba6f5953bb66be737
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-19 17:12:01 -0300

    Add Team 👯 + Player 🧍‍♀️ to admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Area, Competition
+from .models import Area, Competition, Player, Team
 
 
 @admin.register(Competition)
@@ -11,3 +11,13 @@ class CompetitionAdmin(admin.ModelAdmin):
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
     list_display = ("name",)
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name", "tla", "short_name", "address", "area")
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("name", "position", "date_of_birth", "nationality")
