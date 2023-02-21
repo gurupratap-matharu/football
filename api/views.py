11e5b66b37d646d53b725ebfd8c30e49a67bab7d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-20 21:34:12 -0300

    Introduce players + teams list apis

--- api/views.py
+++ api/views.py
@@ -5,8 +5,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 
-from .models import Competition
-from .serializers import CompetitionSerializer
+from .models import Competition, Player, Team
+from .serializers import CompetitionSerializer, PlayerSerializer, TeamSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -27,3 +27,13 @@ class CompetitionAPIView(generics.ListAPIView):
 class CompetitionDetailView(generics.RetrieveAPIView):
     queryset = Competition.objects.all()
     serializer_class = CompetitionSerializer
+
+
+class PlayersAPIView(generics.ListAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+class TeamAPIView(generics.ListAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
