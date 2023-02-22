d7b59da7f4ffbc32206263eb63d4b491aef8a2f2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-22 17:48:31 -0300

    Show teams as inline in competition

--- api/admin.py
+++ api/admin.py
@@ -2,15 +2,27 @@ from django.contrib import admin
 
 from .models import Area, Coach, Competition, Player, Team
 
+admin.site.site_header = "Football ⚽️"
+admin.site.site_title = "Football API Admin Portal"
+admin.site.index_title = "Welcome to Football Admin Portal"
+
+
+class TeamInline(admin.TabularInline):
+    model = Competition.teams.through
+    extra = 0
+    can_delete = False
+
 
 @admin.register(Competition)
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ("name", "code", "area")
+    exclude = ("teams",)
+    inlines = [TeamInline]
 
 
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", "code")
 
 
 @admin.register(Team)
